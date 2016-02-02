67191e568fa3a91c145e53ccc73ce20e4f51a9d3
Andi Milhomme <amilhomme@biscom.com>
2016-02-02 16:35:38 -0500

    First commit, nothing fancy

---
+++ MicroBlog/Modules/MainModule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MicroBlog.Models;
+using Nancy;
+using Nancy.Routing;
+using Nancy.ModelBinding;
+
+namespace MicroBlog.Modules
+{
+    public class MainModule : NancyModule
+    {
+
+
+        public MainModule()
+        {
+            
+            // Routes simply return a view associated with the request
+            // Simple Login, view, write. 
+            // Everything else is simple and self explanatory to understand.
+            
+            
+            Get["/"] = o =>
+
+            {
+                var title = new ShowTitle {Title = "Read"};
+                return View["Views/Home", title];
+
+            };
+            Get["/write"] = o =>
+
+            {
+                var title = new ShowTitle {Title = "Write"};
+                return View["Views/Write", title];
+
+            };
+            Get["/login"] = o =>
+
+            {
+                var title = new ShowTitle {Title = "Login"};
+                return View["Views/Login", title];
+
+            };
+
+
+        }
+    }
+}
---
+++ MicroBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Owin;
+
+namespace MicroBlog
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
