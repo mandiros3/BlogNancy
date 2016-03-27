ba56ee95667b9f32b1368cb6067ec00eda79851b
Andi Milhomme <mandirosa3@gmail.com>
2016-03-27 15:02:43 -0400

    Revert "First unit test pass (Haven't found a good use for them yet, we'll see)"
    This reverts commit 3ef2220297c11aa622718bb960bfd93ef826ca89.

--- BlogNancy.Test/MainModuleTest.cs
+++ BlogNancy.Test/MainModuleTest.cs
@@ -1,7 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using Nancy;
-using Nancy.Diagnostics.Modules;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Nancy.Testing;
 
 namespace MicroBlog.Test
@@ -9,35 +6,11 @@ namespace MicroBlog.Test
     [TestFixture]
     public class MainModuleTest
     {
-        private Browser _browser;
-        [SetUp]
-        public void SetUp()
-        {
-            /* I had to tell Nancy which modules to load. 
-            You can do this by using the configurable bootstrapper, 
-            which gives you an API to configure parts of Nancy yourself. 
-            */
-
-            //Given
-            var bootstrapper = new ConfigurableBootstrapper(with =>
-            {
-                with.Module<MainModule>();
-            });
-            _browser = new Browser(bootstrapper);
-            bootstrapper.Initialise();
-
-        }
-
         [Test]
         public void index_test()
         {
-            var result = _browser.Get("/", with => {
-            with.HttpRequest();
-            });
-            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            
         }
-
-      
 
 
     }
