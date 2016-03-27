cd844f7e931b069375a5c878b013041cdd284bcd
Andi Milhomme <mandirosa3@gmail.com>
2016-03-27 01:28:15 -0400

    Introduced Unit testing to the app. Stubs for now

---
+++ MicroBlog.Test/MainModuleTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Nancy.Testing;
+
+namespace MicroBlog.Test
+{
+    [TestFixture]
+    public class MainModuleTest
+    {
+        [Test]
+        public void index_test()
+        {
+            
+        }
+
+
+    }
+}
